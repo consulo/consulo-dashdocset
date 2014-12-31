def271b05be16bec4af130bb249abae0fe153888
VISTALL <vistall.valeriy@gmail.com>
2014-12-31 12:48:10 +0300

    java impl

---
+++ impl/java-impl/src/org/mustbe/consulo/dashdocset/java/JavaDashKeywordProvider.java
@@ -0,0 +1,31 @@
+package org.mustbe.consulo.dashdocset.java;
+
+import org.jetbrains.annotations.NotNull;
+import org.mustbe.consulo.dashdocset.DashKeywordProvider;
+import com.intellij.lang.Language;
+import com.intellij.openapi.projectRoots.JavaSdkVersion;
+import com.intellij.openapi.projectRoots.JavaSdkVersionUtil;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.ArrayUtil;
+
+/**
+ * @author VISTALL
+ * @since 31.12.14
+ */
+public class JavaDashKeywordProvider implements DashKeywordProvider
+{
+	@NotNull
+	@Override
+	public String[] findKeywords(@NotNull Language language, @NotNull PsiFile psiFile)
+	{
+		JavaSdkVersion javaSdkVersion = JavaSdkVersionUtil.getJavaSdkVersion(psiFile);
+		if(javaSdkVersion == null)
+		{
+			return ArrayUtil.EMPTY_STRING_ARRAY;
+		}
+		// JDK_1_X
+		String name = javaSdkVersion.name();
+		String index = name.substring(6, name.length());
+		return new String[] {"java" + index};
+	}
+}
